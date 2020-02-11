1d6a3e73b5d8304e3178411758ca9460b15796d3
c5-Studios <37878468+c5-Studios@users.noreply.github.com>
2020-02-11 11:56:43 -0600

    Update PluginMain.lua

--- PluginMain.lua
+++ PluginMain.lua
@@ -3,8 +3,20 @@
 local plugin = plugin
 local script = script
 local pluginBin = script.Parent
+local http = game:GetService("HttpService")
+local textureURL = ""
 
 local toolbar = plugin:CreateToolbar("PBR Importer Options")
 local buttons = {
-  toolbar:CreateButton("Import as")
+		toolbar:CreateButton("Blocks","Imports the textures as blocks","rbxassetid://11111"),
+		toolbar:CreateButton("Spheres","Imports the textures as spheres","rbxassetid://11111"),
+		toolbar:CreateButton("Raw","Imports the textures as SurfaceAppearance's","rbxassetid://11111"),
 }
+
+local 
+
+local function import(TYPE)
+	
+end
+
+for i,v in pairs(buttons) do v.Click:Connect(function()import(i)return end) end
